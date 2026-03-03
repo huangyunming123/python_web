ff3882e823c1b0affedf9b0ff21ead8a00d441d3
huangyunming <469000928@qq.com>
2026-03-03 18:28:38 +0800

    Default Changelist

--- 永不放弃篇章/实战案例/京东搜索/京东搜索.js
+++ 永不放弃篇章/实战案例/京东搜索/京东搜索.js
@@ -1,5 +1,26 @@
 var CryptoJS = require('crypto-js')
-require('./source')
+var { ParamsSign } = require('./source')
+
+// 使用解构赋值 var { ParamsSign } = require('./source') 直接导  文件大的时候用
+  window.PSignCom = new ParamsSign({
+            appId: "fb5df",
+            preRequest: !1,
+            onSign: function(e) {
+                0 != e.code && oA.postDraData(704, "ParamsSign签名不可用", "ParamsSign", "code != 0", "", "")
+            },
+            onRequestTokenRemotely: function(e) {
+                e.code,
+                e.message
+            },
+            onRequestToken: function(e) {
+                e.code,
+                e.message
+            }
+        });
+// console.log(window.PSignCom)
+// console.log("-------------------------------------")
+
+
 var params = {
     "enc": "utf-8",
     "area": "1_72_55652_0",
@@ -23,6 +44,7 @@ const paramsH5sign = {
 if (params) {
     paramsH5sign['body'] = CryptoJS.SHA256(JSON.stringify(params))
 }
-console.log(paramsH5sign)
-
- window.PSign.sign(paramsH5sign)
+// console.log(paramsH5sign)
+// console.log("-------------------------------------")
+var result = window.PSignCom._$sdnmd(paramsH5sign);
+console.log(result)
